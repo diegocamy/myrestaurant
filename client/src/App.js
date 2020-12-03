f1010b465b692966504b1d13e66690e1a6b00b67
diegocamy <diegocamyy@gmail.com>
2020-12-03 15:52:09 -0300

    Created the Navbar component with responsive design

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import { Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    <Switch>
-      <Route path='/' exact component={Home} />
-      <Route component={() => <h1>NOT FOUND</h1>} />
-    </Switch>
+    <BrowserRouter>
+      <>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route component={() => <h1>NOT FOUND</h1>} />
+        </Switch>
+      </>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const NavContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: crimson;
+  color: white;
+  padding: 10px 20px;
+
+  .logo {
+    font-size: 1.2rem;
+    span {
+      font-weight: lighter;
+      font-size: 1.3rem;
+    }
+  }
+
+  .navLinks {
+    margin-left: auto;
+
+    ul {
+      display: flex;
+      list-style: none;
+
+      li {
+        margin: 0 10px;
+        text-transform: uppercase;
+
+        a {
+          padding: 10px;
+          text-decoration: none;
+          color: white;
+          border-radius: 20px;
+
+          &:hover {
+            background-color: rgba(255, 255, 255, 0.4);
+          }
+        }
+      }
+    }
+  }
+
+  .hamburger {
+    display: none;
+  }
+
+  .active {
+    background-color: rgba(255, 255, 255, 0.4);
+  }
+
+  @media only screen and (max-width: 800px) {
+    .navLinks {
+      display: none;
+    }
+
+    .hamburger {
+      display: block;
+      margin-left: auto;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+
+    .hamburger-links {
+      position: absolute;
+      top: 55px;
+      width: 100vw;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      background-color: crimson;
+      transition: all 200ms ease-in-out;
+      text-align: center;
+
+      ul {
+        list-style: none;
+        width: 100vw;
+        li {
+          margin: 20px auto;
+          width: 100%;
+          a {
+            min-width: 100px;
+            padding: 10px;
+            text-decoration: none;
+            color: white;
+          }
+
+          i {
+            &:hover {
+              cursor: pointer;
+            }
+          }
+        }
+      }
+    }
+
+    .hide {
+      left: -100vw;
+    }
+
+    .show {
+      left: 0;
+    }
+  }
+`;
+
+const Navbar = (props) => {
+  console.log(props);
+  const [isShowingLinks, setIsShowingLinks] = useState(false);
+  return (
+    <NavContainer>
+      <div className="logo">
+        <h2>
+          <span>my</span>restaurant
+        </h2>
+      </div>
+      <div className="navLinks">
+        <ul>
+          <li>
+            <NavLink to="/products" activeClassName="active">
+              Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart" activeClassName="active">
+              Cart
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" activeClassName="active">
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="hamburger">
+        <i
+          className="fas fa-bars fa-2x"
+          onClick={() => setIsShowingLinks(!isShowingLinks)}
+        ></i>
+      </div>
+      <div className={`hamburger-links ${isShowingLinks ? "show" : "hide"}`}>
+        <ul>
+          <li>
+            <NavLink to="/products" activeClassName="active">
+              Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart" activeClassName="active">
+              Cart
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" activeClassName="active">
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <i
+              className="fas fa-times fa-2x"
+              onClick={() => setIsShowingLinks(!isShowingLinks)}
+            ></i>
+          </li>
+        </ul>
+      </div>
+    </NavContainer>
+  );
+};
+
+export default Navbar;

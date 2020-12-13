eb3425b384755f6db8229d50e28da5595d2e5af0
dcamy <diegocamyy@gmail.com>
2020-12-13 14:20:48 -0300

    Added CartTotal section to Cart page, refactor to use react-redux hooks and some changes to css of Cart Page

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -13,6 +13,7 @@ const Item = styled.div`
   .description {
     display: flex;
     align-items: center;
+    margin-left: 10px;
 
     img {
       width: 150px;
@@ -23,9 +24,27 @@ const Item = styled.div`
   }
 
   .buttons {
+    border: 1px solid #f2f2f2;
+    width: 70%;
+    margin: auto;
     display: flex;
+    border-left: none;
+    border-right: none;
     align-items: center;
-    justify-content: space-around;
+    justify-content: space-between;
+
+    button {
+      border: 1px solid #f2f2f2;
+      border-top: none;
+      border-bottom: none;
+      padding: 10px 12px;
+      background-color: #fafafa;
+      color: black;
+      font-weight: bold;
+      &:hover {
+        cursor: pointer;
+      }
+    }
   }
 
   @media only screen and (max-width: 700px) {
@@ -33,9 +52,20 @@ const Item = styled.div`
     grid-template-columns: auto 19% 19% 19%;
 
     .description {
+      margin-left: none;
+      flex-direction: column;
+      align-items: center;
       img {
-        width: 50%;
+        height: 70px;
+        width: 120px;
         margin-right: 5px;
+      }
+    }
+
+    .buttons {
+      width: 100%;
+      button {
+        padding: 5px;
       }
     }
   }
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 
 import CartItem from "../components/CartItem";
 
@@ -13,16 +13,22 @@ const Container = styled.div`
   padding-top: 30px;
   max-width: 1100px;
   min-height: 100vh;
+
+  @media only screen and (max-width: 700px) {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
 `;
 
 const CartItemsList = styled.div`
   .top {
     display: grid;
-    height: 100px;
+    height: 50px;
     align-items: center;
     grid-template-columns: auto 15% 15% 15%;
     margin-bottom: 5px;
-    background-color: white;
+    background-color: crimson;
+    color: white;
     text-align: center;
 
     h4 {
@@ -43,7 +49,96 @@ const CartItemsList = styled.div`
   }
 `;
 
-const Cart = ({ cart }) => {
+const CartTotal = styled.div`
+  width: 30%;
+  margin-left: auto;
+
+  .grid {
+    background-color: white;
+    display: grid;
+    grid-template-columns: repeat(2, 50%);
+    height: 100px;
+    align-items: center;
+    border-top: 3px solid crimson;
+    margin-top: 25px;
+
+    h4 {
+      &:nth-child(odd) {
+        margin-left: 10px;
+      }
+
+      &:nth-child(even) {
+        margin-left: auto;
+        margin-right: 10px;
+        font-weight: normal;
+      }
+    }
+  }
+
+  .checkout-button {
+    border: none;
+    width: 100%;
+    margin: 15px auto;
+    background-color: crimson;
+    color: white;
+    border-radius: 30px;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 250ms ease-in-out;
+
+    i:first-child {
+      margin-left: auto;
+      margin-right: 5px;
+    }
+    i:last-child {
+      margin-left: auto;
+      margin-right: 5px;
+    }
+
+    &:hover {
+      cursor: pointer;
+      background-color: #e72a50;
+      transform: scale(1.05);
+    }
+  }
+
+  @media only screen and (max-width: 700px) {
+    width: 100%;
+    margin: auto;
+  }
+`;
+
+const Cart = () => {
+  const cart = useSelector((state) => state.cart.cart);
+  const [subtotal, setSubtotal] = useState(0);
+  const [iva, setIva] = useState(0);
+
+  const calculateSubTotal = (items) => {
+    return items.reduce((acc, curr) => {
+      if (curr.sale_price) {
+        acc = acc + curr.sale_price * curr.qty;
+      } else {
+        acc = acc + curr.regular_price * curr.qty;
+      }
+
+      return acc;
+    }, 0);
+  };
+
+  const calculateIva = (value) => value * 0.22;
+
+  const formatNumber = (num) => (Math.round(num * 100) / 100).toFixed(2);
+
+  useEffect(() => {
+    setSubtotal(calculateSubTotal(cart));
+  }, [setSubtotal, cart]);
+
+  useEffect(() => {
+    setIva(calculateIva(subtotal));
+  }, [subtotal, setIva]);
+
   return (
     <CartPage>
       <Container>
@@ -55,18 +150,27 @@ const Cart = ({ cart }) => {
             <h4>Precio Total</h4>
           </div>
           {cart.map((item) => (
-            <CartItem item={item} />
+            <CartItem key={item.id} item={item} />
           ))}
         </CartItemsList>
+        <CartTotal>
+          <div className="grid">
+            <h4>Subtotal</h4>
+            <h4>$ {formatNumber(subtotal)}</h4>
+            <h4>IVA</h4>
+            <h4>$ {formatNumber(iva)}</h4>
+            <h4>Total</h4>
+            <h4>$ {formatNumber(subtotal + iva)}</h4>
+          </div>
+          <button className="checkout-button">
+            <i className="fas fa-shopping-cart"></i>
+            <h3>Finalizar Pedido</h3>
+            <i className="fas fa-arrow-right"></i>
+          </button>
+        </CartTotal>
       </Container>
     </CartPage>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    cart: state.cart.cart,
-  };
-};
-
-export default connect(mapStateToProps)(Cart);
+export default Cart;

c699fe4da77617c2766bf925e3de149f4c3d585b
dcamy <diegocamyy@gmail.com>
2021-01-17 19:47:34 -0300

    Finished the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 import friends from "../assets/backgrounds/friends.jpg";
 
@@ -31,34 +32,53 @@ const ColoredBox = styled.div`
   display: flex;
   flex-direction: column;
   width: 400px;
-  height: 400px;
+  height: 540px;
   background-color: white;
   z-index: 3;
 
   .top {
     background-color: crimson;
-    height: 40%;
+    height: 35%;
 
     h2 {
       text-align: center;
       color: white;
-      font-size: 2rem;
-      margin-top: 15px;
+      font-size: 3rem;
+      margin-top: 30px;
       span {
         font-weight: normal;
-        font-size: 1.3rem;
+        font-size: 2.1rem;
       }
+    }
+
+    h4 {
+      text-align: center;
+      color: white;
+      font-size: 1.5rem;
     }
   }
   .bottom {
     background-color: #fafafa;
-    height: 60%;
+    height: 65%;
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+
+    a {
+      text-decoration: none;
+      color: crimson;
+      margin: 20px;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
   }
 `;
 
 const LoginBox = styled.div`
   position: absolute;
-  top: 20%;
+  top: 27%;
   left: 0;
   right: 0;
   margin: auto;
@@ -70,8 +90,98 @@ const LoginBox = styled.div`
   display: flex;
   flex-direction: column;
 
-  h2 {
+  .google {
+    display: flex;
+    align-items: center;
+    background-color: #4285f4;
+    color: white;
+    padding: 10px;
+    width: 85%;
+    border: none;
+    border-radius: 3px;
+    font-size: 1.2rem;
     margin: 10px auto;
+    transition: all 300ms ease-in-out;
+
+    &:hover {
+      cursor: pointer;
+      background-color: #2f6bcc;
+    }
+
+    .left {
+      width: 12%;
+      font-size: 1.2rem;
+    }
+  }
+
+  .input-div {
+    text-align: center;
+    position: relative;
+
+    i {
+      position: absolute;
+      top: 50%;
+      left: 11%;
+      color: crimson;
+      font-size: 1.2rem;
+      transform: translateY(-50%);
+    }
+
+    input {
+      border: 2px solid crimson;
+      border-radius: 3px;
+      margin: 10px auto;
+      padding: 10px;
+      font-weight: 450;
+      font-size: 1.2rem;
+      padding-left: 35px;
+      width: 85%;
+    }
+  }
+
+  .divider {
+    margin: 15px 0;
+    text-align: center;
+    position: relative;
+    height: 15px;
+    display: flex;
+    align-items: center;
+
+    hr {
+      position: relative;
+      width: 80%;
+      border: 0.5px solid lightgray;
+      margin: auto;
+    }
+
+    p {
+      background-color: white;
+      position: absolute;
+      left: 0;
+      right: 0;
+      margin: auto;
+      width: 10%;
+    }
+  }
+
+  form {
+    text-align: center;
+    button {
+      color: white;
+      background-color: crimson;
+      border: none;
+      border-radius: 3px;
+      padding: 10px 20px;
+      font-size: 1.2rem;
+      margin: 15px auto;
+      text-align: center;
+      transition: all 300ms ease-in-out;
+
+      &:hover {
+        cursor: pointer;
+        background-color: #ee1742;
+      }
+    }
   }
 `;
 
@@ -84,15 +194,35 @@ const Login = () => {
           <h2>
             <span>my</span>restaurant
           </h2>
+          <h4>Iniciar Sesión</h4>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <Link to="/forgot-password">Olvidaste tu contraseña?</Link>
+        </div>
         <LoginBox className="shadow">
-          <h2>Iniciar Sesión</h2>
-          <label htmlFor="email">Correo Electrónico</label>
-          <input type="text" id="email" />
-          <label htmlFor="password">Contraseña</label>
-          <input type="password" id="password" />
-          <button>Ingresar</button>
+          <div className="google">
+            <div className="left">
+              <i className="fab fa-google"></i>
+            </div>
+            <p>Usando Google</p>
+          </div>
+          <div className="divider">
+            <hr />
+            <p>o</p>
+          </div>
+          <form>
+            <div className="input-div">
+              <i className="fas fa-envelope"></i>
+              <input type="text" id="email" placeholder="Correo Electrónico" />
+            </div>
+            <div className="input-div">
+              <i className="fas fa-unlock-alt"></i>
+              <input type="password" id="password" placeholder="Contraseña" />
+            </div>
+            <button>
+              Ingresar <i className="fas fa-sign-in-alt"></i>
+            </button>
+          </form>
         </LoginBox>
       </ColoredBox>
     </BackGround>

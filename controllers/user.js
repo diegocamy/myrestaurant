0b149deb56722b0466f0b1f221dc2b9bb3db26b2
dcamy <diegocamyy@gmail.com>
2021-03-11 14:39:53 -0300

    Added google oauth login and server now responds with a http only cookie with the jwt token inside

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,9 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
-const jwt = require("../helpers/jwt");
+const { signToken } = require("../auth/jwt");
 const yup = require("yup");
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client({});
 
 const userSchema = yup.object({
   email: yup
@@ -41,9 +43,13 @@ module.exports = {
 
       await newUser.save();
 
-      const token = jwt.signToken(newUser);
+      const token = signToken(newUser);
 
-      res.send(token);
+      res
+        .cookie("jwt", token, {
+          httpOnly: true,
+        })
+        .send();
     } catch (error) {
       next(error);
     }
@@ -67,9 +73,65 @@ module.exports = {
         throw new Error("Email o contraseñá incorrecta");
       }
 
-      const token = jwt.signToken(foundUser);
+      const token = signToken(foundUser);
 
-      res.send(token);
+      res
+        .cookie("jwt", token, {
+          httpOnly: true,
+        })
+        .send();
+    } catch (error) {
+      next(error);
+    }
+  },
+  loginWithGoogle: async (req, res, next) => {
+    try {
+      const { tokenId } = req.body;
+
+      const data = await client.verifyIdToken({
+        idToken: tokenId,
+        audience: process.env.GOOGLE_CLIENT_ID,
+      });
+
+      const { email, name } = data.getPayload();
+
+      const foundUser = await User.findOne({ email });
+
+      if (!foundUser) {
+        //crear nuevo usuario y logearlo
+
+        //genera un password aleatorio ya que el modelo User tiene el campo password como required
+        const password =
+          Math.random().toString(36).slice(2) +
+          Math.random().toString(36).slice(2);
+
+        const salt = await bcrypt.genSalt(12);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+          email,
+          name,
+          password: hashedPassword,
+        });
+
+        await newUser.save();
+
+        const token = signToken(newUser);
+
+        return res
+          .cookie("jwt", token, {
+            httpOnly: true,
+          })
+          .send();
+      }
+
+      const token = signToken(foundUser);
+
+      res
+        .cookie("jwt", token, {
+          httpOnly: true,
+        })
+        .send();
     } catch (error) {
       next(error);
     }
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/user");
 
 route.post("/signup", userController.signup);
 route.post("/login", userController.loginWithEmail);
+route.post("/googlelogin", userController.loginWithGoogle);
 
 module.exports = route;
